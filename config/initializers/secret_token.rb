f3f625cfdc18b70d4a055c1839f0d7dc9d9336c9
isaraporn <ikulkumj@andrew.cmu.edu>
2011-09-24 11:04:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouristAttractionInThailand::Application.config.secret_token = '6df815888674d7bcf6eb914b129736388b7aab8e81ea76440c736f40079c85f51d8b930fb079110fbd6dac3930ff7f5df1ae83d9cd7d0e9b46cb25ee25bc01a8'
